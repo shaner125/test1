7d416d4863a6f0b34c044b1f69ad37a8bb1a3958
James Curran <x15758929@student.ncirl.ie>
2017-02-10 08:22:11 +0000

    Login/Register hooked up to server. Pushing data now

--- app/src/main/java/com/example/x15745561/test1/LoginActivity.java
+++ app/src/main/java/com/example/x15745561/test1/LoginActivity.java
@@ -1,7 +1,20 @@
 package com.example.x15745561.test1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +22,62 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+
+        final Button bSignIn = (Button) findViewById(R.id.bSignIn);
+
+        final TextView registerLink = (TextView) findViewById(R.id.tvRegisterLink);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response){
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+                                String name = jsonResponse.getString("name");
+                                int age = jsonResponse.getInt("age");
+
+                                Intent intent = new Intent(LoginActivity.this, UserAreaActivity.class);
+                                intent.putExtra("name", name);
+                                intent.putExtra("username", username);
+                                intent.putExtra("age", age);
+
+                                LoginActivity.this.startActivity(intent);
+
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/x15745561/test1/LoginRequest.java
@@ -0,0 +1,29 @@
+package com.example.x15745561.test1;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by James on 09/02/2017.
+ */
+
+
+public class LoginRequest extends StringRequest {
+    private static final String LOGIN_REQUEST_URL = "http://jamescuran.ie/app/Login.php";
+    private Map<String, String> params;
+
+    public LoginRequest(String username, String password, Response.Listener<String> listener) {
+        super(Method.POST, LOGIN_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
